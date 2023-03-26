b1371fa268764ab3fb4c9f1f8793037db7919eb0
ivan <ivan-guita@hotmail.com>
2023-03-26 09:44:50 -0300

    rederização condicional com EJS compreendida e implementada

--- index.js
+++ index.js
@@ -6,12 +6,26 @@ app.set('view engine', 'ejs');
 
 
 
-app.get('/',function(req,res){
-    //neste caso para renderizar o index.ejs não é necessarios colocar o caminho 
+app.get('/:nome/:lang',function(req,res){
+   
+    
+    //poegando os valores que o usuario enviou 
+
+    var nome =req.params.nome;
+    var lang = req.params.lang;
+
+     //neste caso para renderizar o index.ejs não é necessarios colocar o caminho 
     //porque o render ja espéra que exista a pasta views e busca dentro dela qualçquer aruivo com o nome index
     //que eu escrevi dentro da função 
 
-    res.render('index');
+    res.render('index',{
+        //passando as variaveis para o ejs mostrar no html
+        nome:nome,
+        lang:lang,
+        empresa:"Udemy",
+        inscritos: 8000,
+        msg: true
+    });
     
 });
 
